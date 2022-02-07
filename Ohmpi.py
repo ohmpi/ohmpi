d9670374e1666256aa2130b106ce54658e5a86cf
Guillaume <sagitta1618@gmail.com>
2022-02-07 19:52:54 +0100

    adding materials for dashboard

--- Ohmpi.py
+++ Ohmpi.py
@@ -15,41 +15,50 @@ print('OhmPi start' )
 print('Vers: 1.51')
 print('Import library')
 
+import os
+import sys
+import json
+import glob
+import numpy as np
+import pandas as pd
+import time
+from datetime import datetime
+from termcolor import colored
 
-
-import time , board, busio, numpy, os, sys, json, glob,os.path,adafruit_tca9548a
+"""
+import board, busio,adafruit_tca9548a
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-from pandas import DataFrame
-from datetime import datetime
 from adafruit_mcp230xx.mcp23008 import MCP23008
 from adafruit_mcp230xx.mcp23017 import MCP23017
 import digitalio
 from digitalio import Direction
 from gpiozero import CPUTemperature
+"""
 current_time = datetime.now()
 print(current_time.strftime("%Y-%m-%d %H:%M:%S"))
 """
 Hardware parameters
 """
+
 R_shunt = 0.2# reference resistance value in ohm
-Imax=4800/50/2
+Imax = 4800/50/2
 print(colored('The maximum current cannot be higher than 48 mA', 'red'))
 coef_p2 = 2.50# slope for current conversion for ADS.P2, measurement in V/V
 coef_p3 = 2.50 # slope for current conversion for ADS.P3, measurement in V/V
 offset_p2= 0
 offset_p3= 0
 integer=10
-meas=numpy.zeros((3,integer))
+meas=np.zeros((3,integer))
 
 """
 Import parameters
 """
 
-with open('ohmpi_param.json') as json_file:
-    pardict = json.load(json_file)
+#with open('ohmpi_param.json') as json_file:
+#    pardict = json.load(json_file)
 
-
+"""
 i2c = busio.I2C(board.SCL, board.SDA) #activation du protocle I2C
 mcp = MCP23008(i2c, address=0x20) #connexion I2C MCP23008, injection de courant
 ads_current = ADS.ADS1115(i2c, gain=16,data_rate=860, address=0X48)# connexion ADS1115, pour la mesure de courant
@@ -68,11 +77,12 @@ Elec_A= adafruit_tca9548a.TCA9548A(i2c, 0X76)
 Elec_B= adafruit_tca9548a.TCA9548A(i2c, 0X71)
 Elec_M= adafruit_tca9548a.TCA9548A(i2c, 0X74)
 Elec_N= adafruit_tca9548a.TCA9548A(i2c, 0X70)
-    
+"""    
 
 
 """
 functions
+"""
 """
 # function swtich_mux select the right channels for the multiplexer cascade for electrodes A, B, M and N.
 def switch_mux_on(quadripole):
@@ -146,42 +156,6 @@ def ZERO_mux(nb_elec):
             mcp2.get_pin(a-1).direction=digitalio.Direction.OUTPUT
             mcp2.get_pin(a-1).value= False
 
-# function to find rows with identical values in different columns
-def find_identical_in_line(array_object):
-    output = []
-    if array_object.ndim == 1:
-        temp = numpy.zeros(4)
-        for i in range(len(array_object)):
-            temp[i] = numpy.count_nonzero(array_object == array_object[i])
-        if any(temp > 1):
-            output.append(0)
-    else:
-        for i in range(len(array_object[:,1])):
-            temp = numpy.zeros(len(array_object[1,:]))
-            for j in range(len(array_object[1,:])):
-                temp[j] = numpy.count_nonzero(array_object[i,:] == array_object[i,j])
-            if any(temp > 1):
-                output.append(i)
-    return output
-# 
-# read quadripole file and apply tests
-def read_quad(filename, nb_elec):
-    output = numpy.loadtxt(filename, delimiter=" ",dtype=int) # load quadripole file
-    # locate lines where the electrode index exceeds the maximum number of electrodes
-    test_index_elec = numpy.array(numpy.where(output > nb_elec))
-    # locate lines where an electrode is referred twice
-    test_same_elec = find_identical_in_line(output)
-    # if statement with exit cases (rajouter un else if pour le deuxième cas du ticket #2)
-    if test_index_elec.size != 0:
-        for i in range(len(test_index_elec[0,:])):
-            print("Error: An electrode index at line "+ str(test_index_elec[0,i]+1)+" exceeds the maximum number of electrodes")
-        sys.exit(1)
-    elif len(test_same_elec) != 0:
-        for i in range(len(test_same_elec)):
-            print("Error: An electrode index is used twice at line " + str(test_same_elec[i]+1))
-        sys.exit(1)
-    else:
-        return output
 
 def run_measurement(nb_stack, injection_deltat, R_shunt, coefp2, coefp3, elec_array):
     start_time=time.time()
@@ -217,8 +191,8 @@ def run_measurement(nb_stack, injection_deltat, R_shunt, coefp2, coefp3, elec_ar
           meas[2,k] = AnalogIn(ads_voltage,ADS.P1).voltage * coefp3*1000 # reading voltage value on ADS channel A2
         pin1.value = False; pin0.value = False# stop current injection
         end_delay=time.time()
-        sum_I=sum_I+(numpy.mean(meas[0,:]))
-        Vmn1=((numpy.mean(meas[1,:]))-(numpy.mean(meas[2,:])))
+        sum_I=sum_I+(np.mean(meas[0,:]))
+        Vmn1=((np.mean(meas[1,:]))-(np.mean(meas[2,:])))
         if (n % 2) == 0:
               sum_Vmn=sum_Vmn-Vmn1
               sum_Ps=sum_Ps+Vmn1
@@ -230,7 +204,7 @@ def run_measurement(nb_stack, injection_deltat, R_shunt, coefp2, coefp3, elec_ar
         time.sleep((end_delay-start_delay)-(end_calc-end_delay)) 
     # return averaged values
 #     cpu= CPUTemperature()
-    output = DataFrame({
+    output = pd.DataFrame({
         "time":[datetime.now()],
         "A":elec_array[0],
         "B":elec_array[1],
@@ -258,6 +232,45 @@ def run_measurement(nb_stack, injection_deltat, R_shunt, coefp2, coefp3, elec_ar
     time.sleep(1)
     return output
 
+"""
+
+# function to find rows with identical values in different columns
+def find_identical_in_line(array_object):
+    output = []
+    if array_object.ndim == 1:
+        temp = np.zeros(4)
+        for i in range(len(array_object)):
+            temp[i] = np.count_nonzero(array_object == array_object[i])
+        if any(temp > 1):
+            output.append(0)
+    else:
+        for i in range(len(array_object[:,1])):
+            temp = np.zeros(len(array_object[1,:]))
+            for j in range(len(array_object[1,:])):
+                temp[j] = np.count_nonzero(array_object[i,:] == array_object[i,j])
+            if any(temp > 1):
+                output.append(i)
+    return output
+
+# read quadripole file and apply tests
+def read_quad(filename, nb_elec):
+    output = np.loadtxt(filename, delimiter=" ",dtype=int) # load quadripole file
+    # locate lines where the electrode index exceeds the maximum number of electrodes
+    test_index_elec = np.array(np.where(output > nb_elec))
+    # locate lines where an electrode is referred twice
+    test_same_elec = find_identical_in_line(output)
+    # if statement with exit cases (rajouter un else if pour le deuxième cas du ticket #2)
+    if test_index_elec.size != 0:
+        for i in range(len(test_index_elec[0,:])):
+            print("Error: An electrode index at line "+ str(test_index_elec[0,i]+1)+" exceeds the maximum number of electrodes")
+        sys.exit(1)
+    elif len(test_same_elec) != 0:
+        for i in range(len(test_same_elec)):
+            print("Error: An electrode index is used twice at line " + str(test_same_elec[i]+1))
+        sys.exit(1)
+    else:
+        return output
+
 # save data
 def append_and_save(path, last_measurement):
     
@@ -274,24 +287,68 @@ def append_and_save(path, last_measurement):
 """
 Main loop
 """
-N=read_quad("dd.txt",pardict.get("nb_electrodes")) # load quadripole file
+import threading
 
-if N.ndim == 1:
-    N = N.reshape(1, 4)
-ZERO_mux(pardict.get("nb_electrodes"))
-for g in range(0,pardict.get("nbr_meas")): # for time-lapse monitoring
+class OhmPi(object):
+    def __init__(self, pardict):
+        self.status = 'idle'
+        self.run = True
+        self.t = None
+        self.pardict = pardict
 
-    for i in range(0,N.shape[0]): # loop over quadripoles
-        # call the switch_mux function to switch to the right electrodes
+    def measure(self):
+        self.run = True
+        self.status = 'running'
 
-        
-        switch_mux_on(N[i,])
-        # run a measurement
-        current_measurement = run_measurement(pardict.get("stack"), pardict.get("injection_duration"), R_shunt, coef_p2, coef_p3, N[i,])
-        switch_mux_off(N[i,])
-        #save data and print in a text file
-        append_and_save(pardict.get("export_path"), current_measurement)
-        print(i+1,'/',N.shape[0])
-    print('end of sequence')
-    ZERO_mux(pardict.get("nb_electrodes"))
-    time.sleep(pardict.get("sequence_delay")) #waiting next measurement (time-lapse)
+        N = read_quad("dd.txt", self.pardict.get("nb_electrodes")) # load quadripole file
+
+        if N.ndim == 1:
+            N = N.reshape(1, 4)
+
+        def func():
+            for g in range(0, self.pardict.get("nbr_meas")): # for time-lapse monitoring
+                if self.run == False:
+                    print('INTERRUPTED')
+                    break
+                t0 = time.time()
+                fname = self.pardict.get("export_path").replace('.csv', '_' + datetime.now().strftime('%Y%m%dT%H%M%S') + '.csv')
+                print('saving to ', fname)
+                print('\r{:d}/{:d}'.format(0, N.shape[0]), end='')
+                #ZERO_mux(self.pardict.get("nb_electrodes"))
+                for i in range(0,N.shape[0]): # loop over quadripoles
+                    if self.run == False:
+                        break
+                    # call the switch_mux function to switch to the right electrodes
+                    #switch_mux_on(N[i,])
+
+                    # run a measurement
+                    #current_measurement = run_measurement(self.pardict.get("stack"), pardict.get("injection_duration"), R_shunt, coef_p2, coef_p3, N[i,])
+                    #switch_mux_off(N[i,])
+                    current_measurement = pd.DataFrame({
+                        'a': [N[i, 0]], 'b': [N[i, 1]], 'm': [N[i, 2]], 'n': [N[i, 3]], 'rho': np.abs(np.random.randn(1))
+                    })
+                    time.sleep(np.abs(np.random.randn(1))[0])
+
+                    # save data and print in a text file
+                    append_and_save(fname, current_measurement)
+                    print('\r{:d}/{:d}'.format(i+1, N.shape[0]), end='')
+                print('end of sequence')
+
+                measuring_time = time.time() - t0
+                sleep_time = self.pardict.get("sequence_delay") - measuring_time
+                if sleep_time < 0:
+                    # it means that the measuring time took longer than the sequence delay
+                    sleep_time = 0
+
+                # sleeping time between sequence (not good now)
+                if self.pardict.get("nbr_meas") > 1:
+                    time.sleep(sleep_time) #waiting next measurement (time-lapse)
+            self.status = 'idle'
+        self.t = threading.Thread(target=func)
+        self.t.start()
+
+    def stop(self):
+        self.run = False
+        if self.t is not None:
+            self.t.join()
+        print('self.status', self.status)
