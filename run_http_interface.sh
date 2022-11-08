7a231c648c7d165ebae787c45a4d9e795fa2ad4e
Guillaume <sagitta1618@gmail.com>
2022-11-08 21:57:04 +0000

    check run_http_interface.sh

--- run_http_interface.sh
+++ run_http_interface.sh
@@ -3,5 +3,5 @@ USER="pi"  # change if other username
 cd /home/$USER/OhmPi
 source /home/$USER/OhmPi/ohmpy/bin/activate
 python ohmpi.py &  # run ohmpi.py to capture the commands
-python http_inteface.py  # run http_interface to serve the web GUI
+python http_interface.py  # run http_interface to serve the web GUI
 
