af01faf62e9d1257b6db4b7bfb2d509e68a880f3
Clement Remi <remi.clement@irstea.fr>
2022-11-13 20:57:48 +0100

    Update ohmpi.py with license information

--- ohmpi.py
+++ ohmpi.py
@@ -2,6 +2,7 @@
 """
 created on January 6, 2020.
 Updates May 2022, Oct 2022.
+Licensed under CERN-OHL-S v2 or any later version
 Ohmpi.py is a program to control a low-cost and open hardware resistivity meter OhmPi that has been developed by
 Rémi CLEMENT (INRAE), Vivien DUBOIS (INRAE), Hélène GUYARD (IGE), Nicolas FORQUET (INRAE), Yannick FARGIER (IFSTTAR)
 Olivier KAUFMANN (UMONS), Arnaud WATELET (UMONS) and Guillaume BLANCHY (FNRS/ULiege).
