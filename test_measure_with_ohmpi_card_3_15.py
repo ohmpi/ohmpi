3bf86471687e40b40cac492ac473869c0cba0284
su530201 <olivier.kaufmann@umons.ac.be>
2023-04-15 21:46:37 +0200

    Fix negative sleep_times

--- test_measure_with_ohmpi_card_3_15.py
+++ test_measure_with_ohmpi_card_3_15.py
@@ -1,7 +1,10 @@
+import numpy as np
 from utils import change_config
 change_config('config_ohmpi_card_3_15.py', verbose=False)
 from OhmPi.measure import OhmPiHardware
 
 k = OhmPiHardware()
-k._vab_pulse(vab=12, length=1., sampling_rate=20., polarity=1)
+k._vab_pulse(vab=12, length=1., sampling_rate=10., polarity=1)
+r = k.reading[:,2]/k.readings[:,1]
+print(f'Mean resistance: {np.mean(r):.3f} Ohms, Dev. {100*np.std(r)/np.mean(r):.1f} %')
 change_config('config_default.py', verbose=False)
