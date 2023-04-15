132f9223f9f4cdfc92efc88c7909c24acc934271
su530201 <olivier.kaufmann@umons.ac.be>
2023-04-15 12:38:00 +0200

    Attempts to fix property issue in TxAbstract init

--- hardware/abstract_hardware.py
+++ hardware/abstract_hardware.py
@@ -29,7 +29,6 @@ class TxAbstract(ABC):
         self._inj_time = None
         self._dps_state = 'off'
         self._adc_gain = 1.
-        self.polarity = polarity
         self.inj_time = inj_time
         self.exec_logger.debug(f'{self.board_name} TX initialization')
 
