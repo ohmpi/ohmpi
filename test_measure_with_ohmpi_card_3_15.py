3753ecccb0034c6845622652907c9147ee445c6a
su530201 <olivier.kaufmann@umons.ac.be>
2023-04-15 21:47:20 +0200

    Fix typo

--- test_measure_with_ohmpi_card_3_15.py
+++ test_measure_with_ohmpi_card_3_15.py
@@ -5,6 +5,6 @@ from OhmPi.measure import OhmPiHardware
 
 k = OhmPiHardware()
 k._vab_pulse(vab=12, length=1., sampling_rate=10., polarity=1)
-r = k.reading[:,2]/k.readings[:,1]
+r = k.readings[:,2]/k.readings[:,1]
 print(f'Mean resistance: {np.mean(r):.3f} Ohms, Dev. {100*np.std(r)/np.mean(r):.1f} %')
 change_config('config_default.py', verbose=False)
