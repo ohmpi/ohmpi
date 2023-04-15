bc6a25f26814893dd1292a49f9ac12cb07c8b3cf
su530201 <olivier.kaufmann@umons.ac.be>
2023-04-15 22:45:41 +0200

    Adds a plot

--- test_measure_with_ohmpi_card_3_15.py
+++ test_measure_with_ohmpi_card_3_15.py
@@ -5,11 +5,8 @@ change_config('config_ohmpi_card_3_15.py', verbose=False)
 from OhmPi.measure import OhmPiHardware
 
 k = OhmPiHardware()
-k._vab_pulse(vab=12, length=1., sampling_rate=10., polarity=1)
+k._vab_pulse(vab=12, length=1., sampling_rate=k.tx.sampling_rate, polarity=1)
 r = k.readings[:,2]/k.readings[:,1]
 print(f'Mean resistance: {np.mean(r):.3f} Ohms, Dev. {100*np.std(r)/np.mean(r):.1f} %')
-fig, ax = plt.subplots()
-ax.plot(k.readings[:,0], k.readings[:, 1], '-r', label='current')
-ax2 = ax.twinx()
-ax.plot(k.readings[:,0], k.readings[:, 2], '-b', label='voltage')
+print(f'sampling rate: {k.tx.sampling_rate:.1f} ms, mean sample spacing: {np.mean(np.diff(k.readings[:,0]))*1000.:.1f} ms')
 change_config('config_default.py', verbose=False)
