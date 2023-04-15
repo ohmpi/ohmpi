3e40559d23de6ec01d2470d9dc76bc9de8da20c4
su530201 <olivier.kaufmann@umons.ac.be>
2023-04-15 20:39:44 +0200

    Attemps to use the sampling_rate in _vab_pulse

--- test_ohmpi_card_3_15.py
+++ test_ohmpi_card_3_15.py
@@ -34,5 +34,5 @@ from utils import change_config
 
 change_config('config_ohmpi_card_3_15.py', verbose=False)
 k = OhmPiHardware()
-k._vab_pulse(vab=12, length=2., sampling_rate=20., polarity=1)
+k._vab_pulse(vab=12, length=1., sampling_rate=100., polarity=1)
 change_config('config_default.py', verbose=False)
