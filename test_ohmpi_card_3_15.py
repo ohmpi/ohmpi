f3237cda1ac28ab0cc6420639037afe2e5dd3d5c
su530201 <olivier.kaufmann@umons.ac.be>
2023-04-15 21:30:28 +0200

    Splits hardware test and measure test for ohmpi card 3.15

--- test_ohmpi_card_3_15.py
+++ test_ohmpi_card_3_15.py
@@ -28,11 +28,3 @@ r = np.array(r)
 print(f'Mean resistance: {np.mean(r):.3f} Ohms')
 print(f'Resistance std: {np.std(r):.3f} Ohms')
 print(f'Dev. {100. * np.std(r)/np.mean(r):.1} %')
-
-from OhmPi.measure import OhmPiHardware
-from utils import change_config
-
-change_config('config_ohmpi_card_3_15.py', verbose=False)
-k = OhmPiHardware()
-k._vab_pulse(vab=12, length=1., sampling_rate=100., polarity=1)
-change_config('config_default.py', verbose=False)
