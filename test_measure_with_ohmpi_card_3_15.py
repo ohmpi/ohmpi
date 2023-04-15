7773497dc453a1bd973dd5815309b46c2068ccd3
su530201 <olivier.kaufmann@umons.ac.be>
2023-04-15 21:37:30 +0200

    Splits hardware test and measure test for ohmpi card 3.15

--- test_measure_with_ohmpi_card_3_15.py
+++ test_measure_with_ohmpi_card_3_15.py
@@ -1,7 +1,7 @@
-from OhmPi.measure import OhmPiHardware
 from utils import change_config
-
 change_config('config_ohmpi_card_3_15.py', verbose=False)
+from OhmPi.measure import OhmPiHardware
+
 k = OhmPiHardware()
-k._vab_pulse(vab=12, length=1., sampling_rate=100., polarity=1)
+k._vab_pulse(vab=12, length=1., sampling_rate=10., polarity=1)
 change_config('config_default.py', verbose=False)
