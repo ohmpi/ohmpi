778488ffe62a14ce2b1b82b85d9ceeaa3532ae54
su530201 <olivier.kaufmann@umons.ac.be>
2023-04-15 22:47:05 +0200

    Checks sampling

--- test_measure_with_ohmpi_card_3_15.py
+++ test_measure_with_ohmpi_card_3_15.py
@@ -8,5 +8,5 @@ k = OhmPiHardware()
 k._vab_pulse(vab=12, length=1., sampling_rate=k.tx.sampling_rate, polarity=1)
 r = k.readings[:,2]/k.readings[:,1]
 print(f'Mean resistance: {np.mean(r):.3f} Ohms, Dev. {100*np.std(r)/np.mean(r):.1f} %')
-print(f'sampling rate: {k.tx.sampling_rate:.1f} ms, mean sample spacing: {np.mean(np.diff(k.readings[:,0]))*1000.:.1f} ms')
+print(f'sampling rate: {k.rx.sampling_rate:.1f} ms, mean sample spacing: {np.mean(np.diff(k.readings[:,0]))*1000.:.1f} ms')
 change_config('config_default.py', verbose=False)
