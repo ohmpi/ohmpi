e0b13c9169754bc9dfee66dab497136fb0ca9c6a
su530201 <olivier.kaufmann@umons.ac.be>
2023-04-23 08:49:14 +0200

    Tests graphical output for testing

--- test_measure_with_ohmpi_card_3_15.py
+++ test_measure_with_ohmpi_card_3_15.py
@@ -1,5 +1,5 @@
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from utils import change_config
 change_config('config_ohmpi_card_3_15.py', verbose=False)
 from OhmPi.measure import OhmPiHardware
@@ -19,6 +19,9 @@ r = k.readings[:,2]/k.readings[:,1]
 print(f'Mean resistance: {np.mean(r):.3f} Ohms, Dev. {100*np.std(r)/np.mean(r):.1f} %')
 print(f'sampling rate: {k.rx.sampling_rate:.1f} ms, mean sample spacing: {np.mean(np.diff(k.readings[:,0]))*1000.:.1f} ms')
 print(r)
-print(f'length of array: {len(r)}, expected length{6000./k.rx.sampling_rate}')
+print(f'length of array: {len(r)}, expected length: {6000./k.rx.sampling_rate}')
 print(k.readings)
+fig, ax = plt.subplots()
+ax.plot(k.readings[:,0], k.readings[:,1], '-r')
+ax.plot(k.readings[:,0], k.readings[:,2], '-b')
 change_config('config_default.py', verbose=False)
