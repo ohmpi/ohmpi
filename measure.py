Fixes issue with naming raspberry_pi controller; Fixes computation of sp correction in test_measure_ohmpi_card_3_15.py

measure.py
@@ -106,7 +106,7 @@ class OhmPiHardware:
                 mean_iab.append(np.mean(self.readings[self.readings[:, 1] == i, 3]))
             mean_vmn = np.array(mean_vmn)
             mean_iab = np.array(mean_iab)
-            print(f'Vmn: {mean_vmn}, Iab: {mean_iab}')
+            # print(f'Vmn: {mean_vmn}, Iab: {mean_iab}')  # TODO: delete me
             sp = np.mean(mean_vmn[np.ix_(polarity==1)] - mean_vmn[np.ix_(polarity==-1)]) / 2
             return sp
 

test_measure_with_ohmpi_card_3_15.py
@@ -40,10 +40,11 @@ fig.legend()
 plt.show()
 # compute resistances corrected for SP
 print(f'SP: {k.sp} mV')
-r = ((k.readings[:,4]-k.sp)/k.readings[:,3])
+r = ((k.readings[:,4]-k.readings[:,2]*k.sp)/k.readings[:,3])
 print(f'Mean resistance with sp correction : {np.mean(r):.3f} Ohms, Dev. {100*np.std(r)/np.mean(r):.1f} %')
 print('\nTesting with pulses')
-r = np.array([(k.pulses[i]['vmn']-k.sp)/k.pulses[i]['iab'] for i in k.pulses.keys()])
-print(f'Mean resistance with sp correction : {np.mean(r):.3f} Ohms, Dev. {100*np.std(r)/np.mean(r):.1f} %')
+r = np.array([np.abs((k.pulses[i]['polarity']*k.pulses[i]['vmn']-k.sp)/k.pulses[i]['iab']) for i in k.pulses.keys()])
+for i in range(len(r)):
+    print(f'Mean resistance with sp correction for pulse{i}: {np.mean(r[i]):.3f} Ohms, Dev. {100*np.std(r[i])/np.mean(r[i]):.1f} %')
 change_config('config_default.py', verbose=False)
 