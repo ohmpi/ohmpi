8532361a5f2fe225f8bfe2a3e1d1af22264658f9
su530201 <olivier.kaufmann@umons.ac.be>
2023-04-26 15:02:41 +0200

    Tries to implement switch_on in mux_2024

--- test_mux_2024.py
+++ test_mux_2024.py
@@ -4,8 +4,8 @@ change_config('config_ohmpi_card_3_15.py', verbose=False)
 from OhmPi.hardware_components.mux_2024_rev_0_0 import Mux, MUX_CONFIG
 from OhmPi.hardware_components import raspberry_pi as controller_module
 
-print(MUX_CONFIG)
-mux = Mux({'controller': controller_module.Controller()})
+MUX_CONFIG['controller'] = controller_module.Controller()
+mux = Mux(MUX_CONFIG)
 mux.switch_one(elec=1, role='M', state='on')
 time.sleep(2)
 mux.switch_one(elec=1, role='M', state='off')
