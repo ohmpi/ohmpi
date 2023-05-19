42beabdc7f4bb2f77a5caba568a2bcf818d46807
su530201 <olivier.kaufmann@umons.ac.be>
2023-05-19 14:27:57 +0200

    Updates update_uml to move generated files into uml_diagrams

--- update_uml.sh
+++ update_uml.sh
@@ -1,3 +1,9 @@
 pyreverse ohmpi -p uml_ohmpi
 dot -Tpng classes_uml_ohmpi.dot -O
 dot -Tpng packages_uml_ohmpi.dot -O
+mv classes_uml_ohmpi.dot uml_diagrams/
+mv classes_uml_ohmpi.dot.png uml_diagrams/
+mv packages_uml_ohmpi.dot uml_diagrams/
+mv packages_uml_ohmpi.dot.png uml_diagrams/
+
+
