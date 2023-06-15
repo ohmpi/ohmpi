b67e3862c1175dbde7d4e1515d6ea1ce81b89158
su530201 <olivier.kaufmann@umons.ac.be>
2023-06-15 18:39:43 +0200

    Checks ohmpi_card sampling rates

--- ohmpi/hardware_system.py
+++ ohmpi/hardware_system.py
@@ -174,9 +174,10 @@ class OhmPiHardware:
             sample += 1
             sleep_time = self._start_time + datetime.timedelta(seconds=sample / sampling_rate) - lap
             if sleep_time.total_seconds() < 0.:
-                for i in range(int(sampling_rate * np.abs(sleep_time.total_seconds()))):
-                    _readings.append([elapsed_seconds(self._start_time), self._pulse, self.tx.polarity, np.nan, np.nan])
-                    sample += 1
+                # for i in range(int(sampling_rate * np.abs(sleep_time.total_seconds()))):
+                    # _readings.append([elapsed_seconds(self._start_time), self._pulse, self.tx.polarity, np.nan, np.nan])
+                    # sample += 1
+                sample += int(sampling_rate * np.abs(sleep_time.total_seconds()))
             else:
                 time.sleep(np.max([0., sleep_time.total_seconds()]))
 
