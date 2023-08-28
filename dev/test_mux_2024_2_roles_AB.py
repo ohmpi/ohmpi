09af767dffbc5dd5e7c7db96d945385e6d03ccac
su530201 <olivier.kaufmann@umons.ac.be>
2023-08-28 10:47:19 +0200

    Adds a test for 2 role mux_2024 board

--- dev/test_mux_2024_2_roles_AB.py
+++ dev/test_mux_2024_2_roles_AB.py
@@ -1,29 +1,29 @@
 import time
 from ohmpi.utils import change_config
 import logging
-change_config('../configs/config_mb_2023_mux_2024.py', verbose=False)
+change_config('../configs/config_mb_2023_mux_2024_2_roles_AB.py', verbose=False)
 from ohmpi.hardware_components.mux_2024_rev_0_0 import Mux, MUX_CONFIG
 from ohmpi.hardware_components import raspberry_pi_i2c as ctl_module
 # from ohmpi.config import HARDWARE_CONFIG
 
-stand_alone_mux = False  # Testing hardware component alone
+stand_alone_mux = True  # Testing hardware component alone
 part_of_hardware_system = False  # Testing hardware component as a part of the hardware system
-within_ohmpi = True # Testing hardware component as a part of the hardware system through the ohmpi object
+within_ohmpi = False  # Testing hardware component as a part of the hardware system through the ohmpi object
+update_mux_config = False
 # Stand alone mux
 if stand_alone_mux:
-    MUX_CONFIG['ctl'] = ctl_module.Ctl()
-    MUX_CONFIG['id'] = 'mux_1'
-    MUX_CONFIG['cabling'] = {(i+8, j) : ('mux_1', i) for j in ['A', 'B', 'M', 'N'] for i in range(1,9)}
+    # MUX_CONFIG['ctl'] = ctl_module.Ctl()
+    # MUX_CONFIG['id'] = 'mux_1'
+    # MUX_CONFIG['cabling'] = {(i+8, j) : ('mux_1', i) for j in ['A', 'B', 'M', 'N'] for i in range(1,9)}
     mux = Mux(**MUX_CONFIG)
-    mux.switch_one(elec=9, role='M', state='on')
+    mux.switch_one(elec=2, role='A', state='on')
     time.sleep(2)
-    mux.switch_one(elec=9, role='M', state='off')
-    mux.switch({'A': [9], 'B': [12], 'M': [10], 'N': [11]}, state='on')
+    mux.switch_one(elec=2, role='A', state='off')
+    mux.switch({'A': [2], 'B': [3]}, state='on')
     time.sleep(8)
     # mux.switch({'A': [1], 'B': [4], 'M': [2], 'N': [3]}, state='off')
     mux.reset()
-    mux.test({'A': [9, 10, 11, 12, 13, 14, 15, 16], 'B': [9, 10, 11, 12, 13, 14, 15, 16],
-              'M': [9, 10, 11, 12, 13, 14, 15, 16], 'N': [9, 10, 11, 12, 13, 14, 15, 16]}, activation_time=.1)
+    mux.test({'A': [i for i in range(1, 17)], 'B': [i for i in range(1, 17)]}, activation_time=.1)
 
 # mux as part of a OhmPiHardware system
 if part_of_hardware_system:
