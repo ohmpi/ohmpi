e010b4e8df3824d7df3303ac4453d8fdeee683ba
su530201 <olivier.kaufmann@umons.ac.be>
2023-09-11 10:33:47 +0200

    Changes the way R and R_std are computed taking sp into account

--- ohmpi/ohmpi.py
+++ ohmpi/ohmpi.py
@@ -459,8 +459,8 @@ class OhmPi(object):
             else:
                 delay = 0.
             x = np.where((self._hw.readings[:, 0] >= delay) & (self._hw.readings[:, 2] != 0))
-            R = np.mean(self._hw.readings[x, 2]*self._hw.readings[x, 4])/np.median(self._hw.readings[x, 3])
-            R_std = 100. * np.std(self._hw.readings[x, 2] * (self._hw.readings[x, 4] - self._hw.sp) / self._hw.readings[x, 3]) / R
+            R = np.mean((self._hw.readings[x, 2] * self._hw.readings[x, 4] - self._hw.sp) / self._hw.readings[x, 3])
+            R_std = 100. * np.std((self._hw.readings[x, 2] * self._hw.readings[x, 4] - self._hw.sp) / self._hw.readings[x, 3]) / R
             d = {
                 "time": datetime.now().isoformat(),
                 "A": quad[0],
