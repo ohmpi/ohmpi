8870f3ff259ab68325450eaa65a2957495528f00
su530201 <olivier.kaufmann@umons.ac.be>
2023-09-26 16:12:16 +0200

    Updates hardware_sytem to handle MUX_CONFIG

--- ohmpi/hardware_system.py
+++ ohmpi/hardware_system.py
@@ -108,9 +108,9 @@ class OhmPiHardware:
             mux_module = importlib.import_module(f'ohmpi.hardware_components.{mux_config["model"]}')
             if isinstance(mux_config['ctl'], dict): ### TODO: is this needed?
                 mux_ctl_module = importlib.import_module(f'ohmpi.hardware_components.{mux_config["ctl"]["model"]}')
-                ctl = mux_ctl_module.Ctl(**mux_config['ctl'])  # (**self.ctl)
+                mux_config['ctl'] = mux_ctl_module.Ctl(**mux_config['ctl'])  # (**self.ctl)
             assert issubclass(type(mux_config['ctl']), CtlAbstract)
-            io = mux_config.pop('io', ctl.connections[mux_config.pop('connection', 'i2c')])
+            io = mux_config.pop('io', mux_config['ctl'].connections[mux_config.pop('connection', 'i2c')])
             mux_config.update({'io': io})
             mux_config['id'] = mux_id
 
