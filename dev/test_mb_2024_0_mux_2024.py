89597efb0bf53c09969054536858c1563baf854d
su530201 <olivier.kaufmann@umons.ac.be>
2023-10-09 12:40:46 +0200

    Searches for run_measurement error

--- dev/test_mb_2024_0_mux_2024.py
+++ dev/test_mb_2024_0_mux_2024.py
@@ -58,7 +58,7 @@ if within_ohmpi:
     #k._hw.rx._bias = -1.38
     #print(f'Resistance: {k._hw.last_rho :.2f} ohm, dev. {k._hw.last_dev:.2f} %, rx bias: {k._hw.rx._bias:.2f} mV')
     # k._hw._plot_readings()
-    A, B, M, N = (28, 25, 27, 26)
+    A, B, M, N = (0, 0, 0, 0)
     # k._hw.switch_mux([A, B, M, N], state='on')
     # k._hw.vab_square_wave(12., cycle_duration=10., cycles=3)
     # k._hw.switch_mux([A, B, M, N], state='off')
