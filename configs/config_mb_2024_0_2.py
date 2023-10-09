5bbb69e907b9c2ce1bece671f0cafb760476ccb9
su530201 <olivier.kaufmann@umons.ac.be>
2023-10-09 11:20:03 +0200

    Updates ohmpi to handle no mux case

--- configs/config_mb_2024_0_2.py
+++ configs/config_mb_2024_0_2.py
@@ -20,13 +20,13 @@ OHMPI_CONFIG = {
 HARDWARE_CONFIG = {
     'ctl': {'model': 'raspberry_pi'},
     'pwr': {'model': 'pwr_batt', 'voltage': 12.},
-    'tx':  {'model': 'mb_2024_rev_0_2',
+    'tx':  {'model': 'mb_2024_0_2',
              'voltage_max': 50.,  # Maximum voltage supported by the TX board [V]
              'current_max': 4800,  # Maximum voltage read by the current ADC on the TX board [mA]
              'r_shunt': 2,  # Shunt resistance in Ohms
              'interface_name': 'i2c'
             },
-    'rx':  {'model': 'mb_2024_rev_0_2',
+    'rx':  {'model': 'mb_2024_0_2',
              'coef_p2': 1.00,  # slope for conversion for ADS, measurement in V/V
              'latency': 0.010,  # latency in seconds in continuous mode
              'sampling_rate': 50,  # number of samples per second
