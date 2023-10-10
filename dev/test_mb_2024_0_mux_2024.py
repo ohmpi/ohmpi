2977eb33f651437a1daa529ac4d33cf932b16a95
su530201 <olivier.kaufmann@umons.ac.be>
2023-10-10 21:41:16 +0200

    Changes test to within standalone

--- dev/test_mb_2024_0_mux_2024.py
+++ dev/test_mb_2024_0_mux_2024.py
@@ -33,10 +33,9 @@ if stand_alone:
                                                                               HARDWARE_CONFIG['rx'].pop(
                                                                                   'interface_name', 'i2c')])})
 
-    print(HARDWARE_CONFIG['rx'])
+    print(HARDWARE_CONFIG['rx'], rx_module)
     rx = rx_module.Rx(**HARDWARE_CONFIG['rx'])
     tx = tx_module.Tx(**HARDWARE_CONFIG['tx'])
-    #ctl = ctl_module.Rx(**HARDWARE_CONFIG['ctl'])
     pwr = pwr_module.pwr(**HARDWARE_CONFIG['pwr'])
 
     tx.polarity = 1
