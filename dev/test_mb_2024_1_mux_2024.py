d75560cfecd23ebafccfb0a52e36c3fae612b092
awatlet <arnaud.watlet@umons.ac.be>
2023-10-12 15:13:14 +0200

    Test_dps

--- dev/test_mb_2024_1_mux_2024.py
+++ dev/test_mb_2024_1_mux_2024.py
@@ -1,7 +1,7 @@
 import matplotlib
 matplotlib.use('TkAgg')
 from ohmpi.utils import change_config
-change_config('../configs/config_mb_2024_0_2__1_mux_2024.py', verbose=False)
+change_config('../configs/config_mb_2024_0_2__1_mux_2024_dps5005.py', verbose=False)
 import importlib
 import time
 import logging
