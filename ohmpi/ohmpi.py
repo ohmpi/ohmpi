342218bf87198c2eff9253a3a5ad6709b74a8557
su530201 <olivier.kaufmann@umons.ac.be>
2023-10-19 19:03:29 +0200

    Updates set A and B to ground to discharge pwr

--- ohmpi/ohmpi.py
+++ ohmpi/ohmpi.py
@@ -576,7 +576,6 @@ class OhmPi(object):
 
             dd['cmd_id'] = str(cmd_id)
             self.data_logger.info(dd)
-            self.switch_mux_on()
             self._hw.switch_mux(electrodes=quad[0:2], roles=['A', 'B'], state='on')
             time.sleep(0.2)
             self._hw.switch_mux(electrodes=quad[0:2], roles=['A', 'B'], state='off')
