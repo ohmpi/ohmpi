95916c5d40a9a2f1563ae01108f1fa90b2381778
su530201 <olivier.kaufmann@umons.ac.be>
2023-10-19 23:59:33 +0200

    Adds constant strategy

--- ohmpi/hardware_system.py
+++ ohmpi/hardware_system.py
@@ -435,7 +435,8 @@ class OhmPiHardware:
             current, voltage = 0., 0.
             diff_vab = np.inf
             if strategy == 'constant':
-                vab_max = tx_volt * 1.001
+                vab_max = tx_volt
+                tx_volt = tx_volt * .9
                 strategy = 'vmax'
             if strategy == 'vmax':
                 while (k < n_steps) and (diff_vab > diff_vab_lim) and (vab_list[k] < vab_max):
