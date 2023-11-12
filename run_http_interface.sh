dd4b281db65471c89797c8d1e88a12f6dcb344d7
jkl <sagitta1618@gmail.com>
2023-11-12 20:30:26 +0100

    Update UI

--- run_http_interface.sh
+++ run_http_interface.sh
@@ -1,5 +1,3 @@
 export PYTHONPATH=`pwd`
 source $PYTHONPATH/ohmpy/bin/activate
-python dev/start_mqtt_html.py &  # run ohmpi.py to capture the commands
-python -m http.server  # run web GUI
-
+python dev/start_mqtt_html.py
