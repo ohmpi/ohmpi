87f7c89ef73905c7120ce3012fea4b03a96895eb
su530201 <olivier.kaufmann@umons.ac.be>
2023-11-28 17:00:02 +0100

    Tries to fix adafruit warning

--- ohmpi/hardware_components/raspberry_pi.py
+++ ohmpi/hardware_components/raspberry_pi.py
@@ -44,14 +44,16 @@ class Ctl(CtlAbstract):
         except RuntimeWarning:
             pass
 
+        warnings.resetwarnings()
         # Extended I2C
         try:
             self.interfaces['i2c_ext'] = ExtendedI2C(4)  # 4 is defined
-        # except RuntimeWarning:
-            # pass
+        except RuntimeWarning:
+            pass
         except Exception as e:
             self.exec_logger.warning(f'Could not initialize Extended I2C:\n{e}')
-        warnings.resetwarnings()
+
+
 
         # modbus
         try:
