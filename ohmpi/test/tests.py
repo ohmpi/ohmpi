2c4f0ce97f4913a4f627c6e8a76b59366f8ff18f
Arnaud Watlet <arnaud.watlet@umons.ac.be>
2023-12-04 11:08:41 +0100

    Updates dg411_gain_ratio test

--- ohmpi/test/tests.py
+++ ohmpi/test/tests.py
@@ -500,21 +500,23 @@ class OhmPiTests():
         # self._hw.switch_mux(quad, roles, state='on')
 
         self._hw.rx._dg411_gain = 1
-        voltages = np.zeros(10)
+        voltages = np.zeros(20)
         for i in range(voltages.shape[0]):
-            time.sleep(.1)
-            voltages[i] = (self._hw.rx.voltage + self._hw.rx._bias) + self._hw.rx._vmn_hardware_offset
+            self._hw.tx.pin_DG0 = True
+            time.sleep(.2)
+            voltages[i] = (self._hw.rx.voltage + self._hw.rx._bias)  # + self._hw.rx._vmn_hardware_offset
 
             # voltages[i] = self._hw.rx.voltage
 
         voltage1 = np.mean(voltages[-5:])
         print(voltages)
         self._hw.rx._dg411_gain = 0.5
-        voltages = np.zeros(10)
+        voltages = np.zeros(20)
         for i in range(voltages.shape[0]):
+            time.sleep(.2)
             self._hw.tx.pin_DG0 = True
             voltages[i] = (self._hw.rx.voltage + self._hw.rx._bias) + self._hw.rx._vmn_hardware_offset
-            time.sleep(.1)
+
             # voltages[i] = self._hw.rx.voltage
             self._hw.tx.pin_DG0 = False
         print(voltages)
