677240a73ad79522a7c687b6b92a8a374db67312
Arnaud Watlet <arnaud.watlet@umons.ac.be>
2023-12-04 16:28:06 +0100

    Updates mux test for i2c_ext_tca

--- ohmpi/test/tests.py
+++ ohmpi/test/tests.py
@@ -39,8 +39,7 @@ HARDWARE_CONFIG_nc = copy.deepcopy(HARDWARE_CONFIG)
 def test_i2c_devices_on_bus(i2c_addr, bus):
     if bus.try_lock():
         i2c_addresses_on_bus = bus.scan()
-        addresses = bus.scan()
-        print([hex(address) for address in addresses if address != 0x70])
+        bus.unlock()
     if i2c_addr in i2c_addresses_on_bus:
         return True
     else:
