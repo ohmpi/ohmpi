1ca451ecfab975ec64429c824b56292d9a933729
Arnaud Watlet <arnaud.watlet@umons.ac.be>
2023-12-04 11:16:25 +0100

    Updates dg411_gain_ratio test

--- ohmpi/test/tests.py
+++ ohmpi/test/tests.py
@@ -500,7 +500,7 @@ class OhmPiTests():
         # self._hw.switch_mux(quad, roles, state='on')
 
         self._hw.rx._dg411_gain = 1
-        voltages = np.zeros(20)
+        voltages = np.zeros(30)
         for i in range(voltages.shape[0]):
             self._hw.tx.pin_DG0 = True
             time.sleep(.2)
@@ -511,9 +511,9 @@ class OhmPiTests():
         voltage1 = np.mean(voltages[-5:])
         print(voltages)
         self._hw.rx._dg411_gain = 0.5
-        voltages = np.zeros(20)
+        voltages = np.zeros(10)
         for i in range(voltages.shape[0]):
-            time.sleep(.2)
+            time.sleep(.1)
             # self._hw.tx.pin_DG0 = True
             voltages[i] = (self._hw.rx.voltage + self._hw.rx._bias)  # + self._hw.rx._vmn_hardware_offset
 
