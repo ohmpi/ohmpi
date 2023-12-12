130878f036fbf11143cf8127024d89f2ccb7a94f
Arnaud Watlet <arnaud.watlet@umons.ac.be>
2023-12-12 11:58:17 +0100

    Tries enabling switch_pwr during injection for polarity 0 on pwr_batt and pwr_dps5005_fixed

--- ohmpi/hardware_components/pwr_dps5005_fixed.py
+++ ohmpi/hardware_components/pwr_dps5005_fixed.py
@@ -2,18 +2,24 @@ from ohmpi.hardware_components.abstract_hardware_components import PwrAbstract
 import numpy as np
 import datetime
 import os
+import time
 from ohmpi.utils import enforce_specs
+from minimalmodbus import Instrument  # noqa
+from ohmpi.hardware_components.pwr_batt import Pwr as Pwr_batt
 
 # hardware characteristics and limitations
 SPECS = {'model': {'default': os.path.basename(__file__).rstrip('.py')},
-         'voltage': {'default': 12., 'max': 12., 'min': 12.},
+         'voltage': {'default': 12., 'max': 50., 'min': 0.},
+         'voltage_min': {'default': 0},
+         'voltage_max': {'default': 0},
+         'current_max': {'default': 60.},
          'current_adjustable': {'default': False},
          'voltage_adjustable': {'default': False},
-         'interface': {'default': 'none'},
+         'pwr_latency': {'default': .5}
          }
 
 
-class Pwr(PwrAbstract):
+class Pwr(Pwr_batt):
     def __init__(self, **kwargs):
         if 'model' not in kwargs.keys():
             for key in SPECS.keys():
@@ -24,31 +30,13 @@ class Pwr(PwrAbstract):
         super().__init__(**kwargs)
         if not subclass_init:
             self.exec_logger.event(f'{self.model}\tpwr_init\tbegin\t{datetime.datetime.utcnow()}')
+        assert isinstance(self.connection, Instrument)
         self._voltage = kwargs['voltage']
+        self._current_max = kwargs['current_max']
+        self.voltage_adjustable = False
+        self.current_adjustable = False
         self._current = np.nan
-        self._switch_pwr_on_zero = True
-        # self._state = 'on'
+        self._pwr_state = 'off'
+        self._pwr_latency = kwargs['pwr_latency']
         if not subclass_init:
             self.exec_logger.event(f'{self.model}\tpwr_init\tend\t{datetime.datetime.utcnow()}')
-
-    @property
-    def current(self):
-        return self._current
-
-    @current.setter
-    def current(self, value, **kwargs):
-        self.exec_logger.debug(f'Current cannot be set on {self.model}')
-
-    @property
-    def voltage(self):
-        return PwrAbstract.voltage.fget(self)
-
-    @voltage.setter
-    def voltage(self, value):
-        PwrAbstract.voltage.fset(self, value)
-
-    # def voltage_pulse(self, voltage=0., length=None, polarity=1):
-    #     self.voltage_pulse(voltage=voltage, length=length, polarity=polarity, switch_pwr=self._switch_pwr_on_zero)
-
-    def inject(self, polarity=1, injection_duration=None):
-        self.inject(polarity=polarity, injection_duration=injection_duration, switch_pwr=self._switch_pwr_on_zero)
