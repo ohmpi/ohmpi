53e7e8544519be5da3f14e282be670cb22e1d3d3
jkl <sagitta1618@gmail.com>
2024-01-30 17:35:39 +0100

    Update doc: move image to software folder

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -57,7 +57,7 @@ exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
-html_logo = '../img/logo/ohmpi/LOGO_OHMPI.png'
+html_logo = 'img/logo/ohmpi/LOGO_OHMPI.png'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
