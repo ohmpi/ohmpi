07867e00a9ea0d0ce1ba74da63acd1aa000024f4
Arnaud Watlet <arnaud.watlet@umons.ac.be>
2024-02-01 17:56:10 +0100

    updates examples

--- examples/example_simple_sequence.py
+++ examples/example_simple_sequence.py
@@ -14,7 +14,11 @@ k.update_settings({"tx_volt": 3.})
 k.update_settings({"nb_stack": 2})
 
 # Set or load sequence
-quad = [1,4,2,3]    # set numpy array of shape (n,4)
+k.sequence = np.array([[1,4,2,3], [2,5,3,4]])    # set numpy array of shape (n,4)
+# k.load_sequence('sequences/ABMN.txt')    # load sequence from a local file
+
+# Run contact resistance check
+k.rs_check()
 
 # Run sequence
-k.run_measurement()
+k.run_sequence()
