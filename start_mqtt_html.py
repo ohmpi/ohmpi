8f9dce2e94dde4aba62e0b06f26f51bbf909d183
Arnaud Watlet <arnaud.watlet@umons.ac.be>
2024-02-28 15:01:25 +0100

    Merges v2024_rc in merge_testing_framework

--- start_mqtt_html.py
+++ start_mqtt_html.py
@@ -5,7 +5,7 @@
 from ohmpi.utils import change_config
 # change_config('../configs/config_mb_2023.py', verbose=False)
 # change_config('../configs/config_mb_2023__4_mux_2023.py', verbose=False)
-#change_config('../configs/config_mb_2024_0_2__4_mux_2023_dps5005.py', verbose=False)
+# change_config('../configs/config_mb_2024_0_2__4_mux_2023_dps5005.py', verbose=False)
 
 # start html interface
 import subprocess
@@ -16,9 +16,9 @@ try:
     from ohmpi.ohmpi import OhmPi
     from ohmpi.config import OHMPI_CONFIG
     k = OhmPi(settings=OHMPI_CONFIG['settings'])
-    import os
-    k.load_sequence(os.path.join(os.path.dirname(__file__), './sequences/wenner16.txt'))
-    k.reset_mux()
+    # import os
+    # k.load_sequence(os.path.join(os.path.dirname(__file__), './sequences/wenner1-16.txt'))
+    # k.reset_mux()
     #k.run_multiple_sequences(sequence_delay=20, nb_meas=3)
 
     if k.controller is not None:
