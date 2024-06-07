a0520d654374fafcfe94b2f1360d1a088636ad3c
Arnaud Watlet <arnaud.watlet@umons.ac.be>
2024-06-07 15:07:02 +0200

    Updates examples for vab_req

--- examples/example_simple_sequence.py
+++ examples/example_simple_sequence.py
@@ -10,7 +10,7 @@ k = OhmPi()
 # Update settings if needed
 k.update_settings({"injection_duration": 1.})
 k.update_settings({"strategy": "constant"})
-k.update_settings({"tx_volt": 3.})
+k.update_settings({"vab_req": 3.})
 k.update_settings({"nb_stack": 2})
 
 # Set or load sequence
