2a10b2ee5effec6d234e8fc40b4c70a697c383fe
Guillaume <sagitta1618@gmail.com>
2024-07-10 08:31:23 +0000

    v2024.0.18

--- ohmpi/ohmpi.py
+++ ohmpi/ohmpi.py
@@ -53,7 +53,7 @@ except Exception as error:
     print(colored(f'Unexpected error: {error}', 'red'))
     arm64_imports = None
 
-VERSION = 'v2024.0.17'
+VERSION = 'v2024.0.18'
 
 
 class OhmPi(object):
