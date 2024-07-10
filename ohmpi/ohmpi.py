885ca52a41cc2e619daf2c1618d8c9d0d814581b
jkl <sagitta1618@gmail.com>
2024-07-10 10:03:44 +0200

    Update to v2024.0.17

--- ohmpi/ohmpi.py
+++ ohmpi/ohmpi.py
@@ -53,7 +53,7 @@ except Exception as error:
     print(colored(f'Unexpected error: {error}', 'red'))
     arm64_imports = None
 
-VERSION = 'v2024.0.16'
+VERSION = 'v2024.0.17'
 
 
 class OhmPi(object):
