87d459a7d942ef5b7261173a78c967c56695d7f4
Arnaud Watlet <arnaud.watlet@umons.ac.be>
2024-11-05 22:41:54 +0100

    Adapt example for 16 electrodes

--- examples/example_simple_sequence.py
+++ examples/example_simple_sequence.py
@@ -14,11 +14,11 @@ k.update_settings({"vab_req": 3.})
 k.update_settings({"nb_stack": 2})
 
 # Set or load sequence
-k.sequence = np.array([[1,4,2,3], [2,5,3,4]])    # set numpy array of shape (n,4)
+k.sequence = np.array([[1+i,4+i,2+i,3+i] for i in range(13)])    # set numpy array of shape (n,4)
 # k.load_sequence('sequences/ABMN.txt')    # load sequence from a local file
 
 # Run contact resistance check
 k.rs_check()
 
 # Run sequence
-k.run_sequence()
+k.run_sequence(fw_in_zip=True)
