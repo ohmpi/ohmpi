edba74d8936bf775b2536eb768fae76ef577f4ca
jkl <sagitta1618@gmail.com>
2025-04-03 21:51:06 +0200

    Add numpy==1.26.4 to install_resipy.sh

--- install_resipy.sh
+++ install_resipy.sh
@@ -4,5 +4,5 @@ which pip
 sudo apt-get install wine --assume-yes
 cd ..
 git clone -b rpi https://gitlab.com/hkex/resipy
-pip install numpy matplotlib scipy pandas requests psutil
+pip install numpy==1.26.4 matplotlib scipy pandas requests psutil
 cd ohmpi
